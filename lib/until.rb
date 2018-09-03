4f2c6377122e5f990fd8a32476bf2df921329a38
Saint-Jean Germila <germila@noukod.com>
2018-09-03 13:45:01 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
-def using_until
-        levitation_force = 6
-        counter = 1
-        until levitation_force == 10
-          puts "Wingardium Leviosa"
-          counter += 1
-        end
-      end
+#def using_until
+ levitation_force = 6
+       counter = 1
+  until levitation_force == 10
+        puts "Wingardium Leviosa"
+        counter += 1
+    end
+  #end
--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,7 @@
 def using_while
 	levitation_force = 6
-	counter = 1
 	while levitation_force < 10
 		puts "Wingardium Leviosa"
-	counter += 1
- end
+	 levitation_force += 1
+  end
 end
